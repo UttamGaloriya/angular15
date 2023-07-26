14ef6e6e8077426b1777b9f4afc5cc392b8bc682
Uttam Galoriya <19ce037@gardividyapith.ac.in>
2023-07-26 10:58:13 +0530

    angular15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'account', loadChildren: () => import('./modules/account/account.module').then(m => m.AccountModule) },
+  { path: 'dashboard', loadChildren: () => import('./modules/dashboard/dashboard.module').then(m => m.DashboardModule) },
+  { path: '', redirectTo: '/account', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/modules/account/account.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AccountRoutingModule } from './account-routing.module';
+import { LoginComponent } from './component/login/login.component';
+import { SignupComponent } from './component/signup/signup.component';
+import { MaterialModule } from 'src/app/shared/material/material.module';
+
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    SignupComponent
+  ],
+  imports: [
+    CommonModule,
+    AccountRoutingModule,
+    MaterialModule
+  ]
+})
+export class AccountModule { }
